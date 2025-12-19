9883cbc485c19e58351719fd597088aabe32f347
HSilvaIMPD <henriqueimpd@hotmail.com>
2025-12-19 21:26:12 +0000

    inicio do desafio ST

---
+++ henrique/nivelnovatoST.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+    int main () {
+
+        /// CARTA 01 ///
+        char estado1;
+        char codigo1[3];
+        char cidade1[29];
+        
+
+
+        return 0;
+
+    }
